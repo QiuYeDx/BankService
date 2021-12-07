38995760acbf7aab8192c62bd19b3f79c19431d4
白子涵(秋夜Dx <20205900@stu.neu.edu.cn>
2021-12-07 22:49:28 +0800

    using namespace Qt;

--- quene.cpp
+++ quene.cpp
@@ -48,9 +48,10 @@ User* Quene::pop()
 
 void Quene::output()
 {
-    qDebug()<<"quene"<<Qt::endl;
+    using namespace Qt;
+    qDebug()<<"quene"<<endl;
     for(int i=front;i<rear;i++)
-        qDebug()<<data[i]->ID<<"   "<<data[i]->type<<"  "<<data[i]->status<<Qt::endl;
+        qDebug()<<data[i]->ID<<"   "<<data[i]->type<<"  "<<data[i]->status<<endl;
 
 }
 
