f594aba4b691f911cfdc5ebc42023f5af22bb302
白子涵(秋夜Dx <20205900@stu.neu.edu.cn>
2021-12-09 17:44:04 +0800

    提供多业务窗口

--- startservice.cpp
+++ startservice.cpp
@@ -12,3 +12,19 @@ StartService::~StartService()
 {
     delete ui;
 }
+
+StartService::StartService(int ID):
+    ID(ID)
+{
+    this->setWindowTitle("银行系统 · 业务");
+    this->setFixedSize(300, 300);
+    //this->setAttribute(Qt::WA_DeleteOnClose, true);
+
+    label_1 = new QLabel(this);
+    label_1->setGeometry(80, 120, 120, 30);
+    label_1->setText("窗口号:");
+
+    label_2 = new QLabel(this);
+    label_2->setGeometry(140, 120, 120, 30);
+    label_2->setText(QString::number(ID));
+}
--- startservice.h
+++ startservice.h
@@ -2,7 +2,7 @@
 #define STARTSERVICE_H
 
 #include <QMainWindow>
-
+#include <QLabel>
 namespace Ui {
 class StartService;
 }
@@ -10,13 +10,15 @@ class StartService;
 class StartService : public QMainWindow
 {
     Q_OBJECT
-
 public:
+    StartService(int ID);
     explicit StartService(QWidget *parent = nullptr);
     ~StartService();
 
 private:
     Ui::StartService *ui;
+    int ID;
+    QLabel *label_1, *label_2;
 };
 
 #endif // STARTSERVICE_H
